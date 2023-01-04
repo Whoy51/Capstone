8d3d42a7860c9a11f1bc9dcdc77ef1a5b298463f
Wesley Hoy <whoy@ey3.tech>
2023-01-04 15:14:02 -0500

    Learning jinga and some SQL practices

--- main.py
+++ main.py
@@ -11,24 +11,25 @@ cur.execute("CREATE TABLE IF NOT EXISTS person(name, age)")
 name = input("Name of person: ")
 age = input("Age of person: ")
 
-cur.execute("""INSERT INTO person VALUES ('""" + name + """', """ + age + """)""")
+cur.executemany("""INSERT INTO person VALUES (?, ?)""", [(name, age)])
 
 con.commit()
 
 res = cur.execute("SELECT name FROM person")
 
-print(res.fetchall())
+names = res.fetchall()
 
 res = cur.execute("SELECT age FROM person")
 
-print(res.fetchall())
+age = res.fetchall()
 
-age = res.fetchone()
 
+print(names)
+print(age)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', name=name, db=[names, age], names=names)
 
 
 if __name__ == '__main__':
